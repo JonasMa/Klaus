635c0db8d35cfcaa66bf7c03171f2edcdf6e8a08
Alex Knittel <Alex@mareikes-mac.fritz.box>
2017-01-18 15:07:07 +0100

    Make Storyboard magically disappear!

--- Klaus/Klaus/MainTabBarController.swift
+++ Klaus/Klaus/MainTabBarController.swift
@@ -10,13 +10,36 @@ import UIKit
 
 class MainTabBarController: UITabBarController {
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated);
+        
+        
+        
+        let tabOne = UINavigationController()
+        let tabTwo = UINavigationController();
+        
+        let tabOneBarItem = UITabBarItem(title: "Profil", image: UIImage(named: "face"), tag: 0);
+        let tabTwoBarItem = UITabBarItem(title: "Gegner", image: UIImage(named: "knife"), tag: 1);
+        
+        tabOne.tabBarItem = tabOneBarItem;
+        tabTwo.tabBarItem = tabTwoBarItem;
+        
+        self.viewControllers = [tabOne,tabTwo];
+        let playerProfileViewController = PlayerProfileViewController();
+        tabOne.pushViewController(playerProfileViewController, animated: true)
+
+        let enemyListViewController = EnemyListViewController();
+        tabTwo.pushViewController(enemyListViewController, animated: true);
+
+    }
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
-        
         UITabBar.appearance().tintColor = Style.accentColor;
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
