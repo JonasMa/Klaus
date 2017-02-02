eedc9a2d693046b56df3d92d65f46a36bef3f83d
Jonas <jonasponas@Natanjas-MBP.fritz.box>
2017-02-02 06:40:03 +0100

    final config

--- Klaus/Klaus/Config.swift
+++ Klaus/Klaus/Config.swift
@@ -11,17 +11,17 @@ import UIKit
 
 struct Config {
     
-    static var axeBasePointsPerSecond = 4;
-    static var coffeeBasePointsPerSecond = 5;
-    static var seitenschneidBasePointsPerSecond = 6;
-    static var alarmBasePointsPerSecond = 7;
+    static let axeBasePointsPerSecond = 4;
+    static let coffeeBasePointsPerSecond = 5;
+    static let seitenschneidBasePointsPerSecond = 6;
+    static let alarmBasePointsPerSecond = 7;
     
-    static var scoreToLevelUpBase = 500;
+    static let scoreToLevelUpBase = 500;
     
-    static var stealBonus = 200.0;
-    static var stealPenalty = 100.0;
+    static let stealBonus = 200.0;
+    static let stealPenalty = 100.0;
     
-    static var possibleColors = [UIColor(red:0.78, green:0.00, blue:0.49, alpha:1.0),
+    static let possibleColors = [UIColor(red:0.78, green:0.00, blue:0.49, alpha:1.0),
                                  UIColor(red:0.05, green:0.29, blue:0.73, alpha:1.0),
                                  UIColor(red:0.12, green:0.61, blue:0.00, alpha:1.0),
                                  UIColor(red:0.90, green:0.38, blue:0.08, alpha:1.0)]
